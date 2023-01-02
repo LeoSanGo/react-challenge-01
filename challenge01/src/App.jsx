821c8d996958899b0e711a1c03645ef557bee7ef
leosango <souoleo@gmail.com>
2023-01-02 06:02:40 -0300

    feat: catch dot mouse position clicked

--- challenge01/src/App.jsx
+++ challenge01/src/App.jsx
@@ -5,28 +5,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleDotOnClick = (e) => {
+    const dotPosition = {
+      top: e.pageX,
+      left: e.pageY,
+    }
+    console.log(dotPosition);
+  }
+
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <span className='dot'onClick={handleDotOnClick} style={{}} />
     </div>
   )
 }
