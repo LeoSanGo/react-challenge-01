a5f0c6c36912fc0cc60ed0415f5ab44defd36184
leosango <souoleo@gmail.com>
2023-01-04 21:03:56 -0300

    feat: implement redo button

--- challenge01/src/App.jsx
+++ challenge01/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [dotList, setDotList] = useState([]);
+  const [dotListRedo, setDotListRedo] = useState([]);
 
   const handleDotOnClick = (e) => {
     const dotPosition = {
@@ -11,22 +12,45 @@ function App() {
       clientY: e.clientY,
     };
     setDotList((prev) => [...prev, dotPosition]);
+    setDotListRedo([]);
   };
 
   const handleUndo = (e) => {
     e.stopPropagation();
+
+    if (dotList.length === 0) return;
     console.log('undo');
+
+    const lastItem = dotList[dotList.length - 1];
+    setDotListRedo((prev) => [...prev, lastItem]);
+
     setDotList((prev) => {
       const copyArray = [...prev].slice(0, -1);
       return copyArray;
     });
   };
 
+  const handleRedo = (e) => {
+    e.stopPropagation();
+
+    if (dotListRedo.length === 0) return;
+    console.log('redo');
+    const lastItem = dotListRedo[dotListRedo.length - 1];
+
+    setDotListRedo((prev) => {
+      const copyArray = [...prev].slice(0, -1);
+      return copyArray;
+    });
+    setDotList((prev) => [...prev, lastItem]);
+  };
+
   return (
     <div className="App" onClick={handleDotOnClick}>
       <button onClick={handleUndo}>Ctrl+Z</button>
+      <button onClick={handleRedo}>Ctrl+Y</button>
       {dotList.map((item) => (
         <span
+        key={item.clientX + item.clientY}
           className="dot"
           style={{ top: item.clientY, left: item.clientX }}
         />
